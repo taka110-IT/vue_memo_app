bfe21ac5bd7e8b5c0a329617e32c82af3de82460
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-02-13 00:40:13 +0900

    一覧表示を実装

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>Memo App</h1>
+<ItemList />
+</template>
+
+<script>
+import ItemList from './components/ItemList.vue'
+
+export default {
+  name: 'App',
+  components: { ItemList }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      <p @click="edit">{{ firstLine(item.body) }}</p>
+    </li>
+    <li @click="edit">＋</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [{id: 1, body: `memo1\nmemo1\nmemo1`},
+              {id: 2, body: `memo2\nmemo2\nmemo2`},
+              {id: 3, body: `memo3\nmemo3\nmemo3`}]
+    }
+  },
+  methods: {
+    edit() {
+      console.log('edit clicked!')
+    }
+  },
+  computed: {
+    firstLine: function() {
+      return function(body) {
+        return String(body.split('\n', 1))
+      }
+    }
+  }
+}
+</script>
+
+<style>
+li {
+  list-style-type: none;
+  text-decoration: underline;
+}
+li:hover {
+  color: blue;
+  cursor: pointer;
+}
+</style>
